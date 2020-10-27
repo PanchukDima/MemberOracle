40a29aae24be4add0d0757d0b873ab8d03a2267f
PanchukDima <panchuk6@gmail.com>
2020-10-27 10:49:37 +0300

    Update calcHeartRisk.sql

--- calcHeartRisk.sql
+++ calcHeartRisk.sql
@@ -1,3 +1,251 @@
+prompt Importing table gp54_admin.risk...
+set feedback off
+set define off
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 4, 3, 7, 8, 9, 10, 12);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 4, 2, 5, 5, 6, 7, 8);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 4, 1, 3, 3, 4, 5, 6);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 4, 0, 2, 2, 5, 3, 4);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 3, 3, 4, 4, 5, 6, 7);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 3, 2, 3, 3, 3, 4, 5);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 3, 1, 2, 2, 2, 3, 3);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 3, 0, 1, 1, 2, 2, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 2, 3, 2, 2, 3, 3, 4);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 2, 2, 1, 2, 2, 2, 3);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 2, 1, 1, 1, 1, 1, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 2, 0, 1, 1, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 1, 3, 1, 1, 1, 2, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 1, 2, 1, 1, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 1, 1, 0, 1, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 1, 0, 0, 0, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 0, 3, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 0, 2, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 0, 1, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 4, 3, 13, 15, 17, 19, 22);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 4, 2, 9, 10, 12, 13, 16);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 4, 1, 6, 7, 8, 9, 11);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 4, 0, 4, 5, 5, 6, 7);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 3, 3, 8, 9, 10, 11, 13);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 3, 2, 5, 6, 7, 8, 9);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 3, 1, 3, 4, 5, 5, 6);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 3, 0, 2, 3, 3, 4, 4);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 2, 3, 4, 5, 5, 6, 7);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 2, 2, 3, 3, 4, 4, 5);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 2, 1, 2, 2, 2, 3, 3);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 2, 0, 1, 1, 2, 2, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 1, 3, 2, 2, 3, 3, 4);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 1, 2, 1, 2, 2, 2, 3);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 1, 1, 1, 1, 1, 1, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 1, 0, 1, 1, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 0, 3, 0, 0, 0, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 0, 2, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 0, 1, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 0, 1, 0, 0, 0, 0, 0, 0, 0);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 4, 3, 14, 16, 19, 22, 26);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 4, 2, 9, 11, 13, 15, 16);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 4, 1, 6, 8, 9, 11, 13);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 4, 0, 4, 5, 6, 7, 9);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 3, 3, 9, 11, 13, 15, 18);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 3, 2, 6, 7, 9, 10, 12);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 3, 1, 4, 5, 6, 7, 9);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 3, 0, 3, 3, 4, 5, 6);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 2, 3, 6, 7, 8, 10, 12);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 2, 2, 4, 5, 6, 7, 8);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 2, 1, 3, 3, 4, 5, 6);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 2, 0, 2, 2, 3, 3, 4);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 1, 3, 4, 4, 5, 6, 7);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 1, 2, 2, 3, 3, 4, 5);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 1, 1, 2, 2, 2, 3, 3);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 1, 0, 1, 1, 2, 2, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 0, 3, 1, 1, 1, 2, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 0, 2, 1, 1, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 0, 1, 0, 1, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 0, 0, 0, 0, 0, 1, 1, 1);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 4, 3, 26, 30, 35, 41, 47);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 4, 2, 18, 21, 25, 29, 34);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 4, 1, 13, 15, 17, 20, 24);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 4, 0, 9, 10, 12, 14, 17);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 3, 3, 18, 21, 24, 28, 33);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 3, 2, 12, 14, 17, 20, 24);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 3, 1, 8, 10, 12, 14, 17);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 3, 0, 6, 7, 8, 10, 12);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 2, 3, 12, 13, 16, 19, 22);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 2, 2, 8, 9, 11, 13, 16);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 2, 1, 5, 6, 8, 9, 11);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 2, 0, 4, 4, 5, 6, 8);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 1, 3, 7, 8, 10, 12, 14);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 1, 2, 5, 6, 7, 8, 10);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 1, 1, 3, 4, 5, 6, 7);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 1, 0, 2, 3, 3, 4, 5);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 0, 3, 2, 2, 3, 3, 4);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 0, 2, 1, 2, 2, 2, 3);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 0, 1, 1, 1, 1, 2, 2);
+
+insert into gp54_admin.risk (KEYID, SEX, SMOKE, AGE, AD, L1, L2, L3, L4, L5)
+values (null, 1, 1, 0, 0, 1, 1, 1, 1, 1);
+
+prompt Done.
+
+
+
+
 CREATE OR REPLACE Function GP54_ADMIN.calc_risk(in_sex in number
                                     ,in_smoke in number
                                     ,in_age in number
